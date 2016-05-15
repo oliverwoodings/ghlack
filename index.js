c607f98f3ab10f1b769b1234795b07af66d74d5e
swagatha-christie <oliver.woodings+swagatha@qubit.com>
2016-05-15 11:26:08 +0100

    More testing foo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('babel-register')
 require('babel-polyfill')
 
 var path = require('path')
+console.log('WHYEYE')
 
 process.on('unhandledRejection', function (reason) {
   process.emit('SIGTERM')
